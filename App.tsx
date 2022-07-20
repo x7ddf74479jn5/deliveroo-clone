38a6f5d60cc4ee587232ad9de7dbff8d4ae6a74d
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-07-20 20:37:11 +0900

    ✨ feat: navigation scafold

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
+import { NativeScreenContainer } from "react-native-screens";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { HomeScreen } from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <TailwindProvider>
-      <View>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </TailwindProvider>
+    <NativeScreenContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NativeScreenContainer>
   );
 }
