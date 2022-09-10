5d4cc79dcce2d59dfea42f7a93fdf484600db654
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-09-11 02:31:59 +0900

    ✨ feat: basket store

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import { Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 import { NativeScreenContainer } from "react-native-screens";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
 
 import { HomeScreen } from "./screens/HomeScreen";
 import { RestaurantScreen } from "./screens/RestaurantScreen";
 import { Restaurant } from "./models";
+import { store } from "./store";
 
 export type RootStackParamList = {
   Home: undefined;
@@ -18,12 +20,14 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
   return (
     <NativeScreenContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-        </Stack.Navigator>
-      </TailwindProvider>
+      <Provider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </Provider>
     </NativeScreenContainer>
   );
 }
---
+++ features/basketSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Dish } from "../models";
+import { RootState } from "../store";
+
+type Basket = {
+  items: Dish[];
+};
+
+const initialState: Basket = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state, action: { payload: Dish }) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex((item) => item.id === action.payload.id);
+      let newBasket = [...state.items];
+
+      if (index > 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`Can't remove produce (id: ${action.payload.id}) as its not in basket! `);
+      }
+      state.items = state.items.filter((item) => item.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+export const selectBasketItems = (state: RootState) => state.basket.items;
+
+export const selectBasketItemsWithId = (state: RootState, id: string) =>
+  state.basket.items.filter((item) => item.id === id);
+export const basketReducer = basketSlice.reducer;
